1a3bc98364b3c2125c7158fc52d635964babc599
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-01-22 13:15:53 +0530

    custom elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useMemo, useState } from 'react'
-import { createEditor } from 'slate'
+import React, { useCallback, useMemo, useState } from 'react'
+import { createEditor, Editor, Transforms } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
+import { CodeElement, DefaultElement } from './Elements'
 import './App.css'
 
 const App = () => {
@@ -12,19 +13,37 @@ const App = () => {
     },
   ])
 
+  const renderElement = useCallback(props => {
+
+    if (props.element.type === 'code') {
+      return <CodeElement {...props} />
+    } else {
+      return <DefaultElement {...props} />
+    }
+
+  }, [])
+
   return (
     <div class="base-div" >
-      <Slate
-        editor={editor}
-        value={value}
-        onChange={newValue => setValue(newValue)}
-      >
-        <Editable 
-          placeholder="Enter some text here..."
+
+      <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+        <Editable
+          renderElement={renderElement}
+          onKeyDown={event => {
+            if (event.key === '`' && event.ctrlKey) {
+              event.preventDefault()
+              Transforms.setNodes(
+                editor,
+                { type: 'code' },
+                { match: n => Editor.isBlock(editor, n) }
+              )
+            }
+          }}
         />
       </Slate>
     </div>
   )
 }
+
 
 export default App;
