54520c371097dbec97b6901de36d1eb9d3d96a79
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-02-06 18:55:00 +0530

    refactored some code

--- src/Leaf.jsx
+++ src/Leaf.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 
-export const BoldLeaf = (props) => {
-    return (
-        <strong {...props.attributes} >{props.children}</strong>
-    )
-}
+// export const BoldLeaf = (props) => {
+//     return (
+//         <strong {...props.attributes} >{props.children}</strong>
+//     )
+// }
 
-export const ItalicLeaf = (props) => {
-    return (
-        <em { ...props.attributes } >{ props.children }</em>
-    )
-}
+// export const ItalicLeaf = (props) => {
+//     return (
+//         <em { ...props.attributes } >{ props.children }</em>
+//     )
+// }
 
-export const UnderlineLeaf = (props) => {
-    return (
-        <u { ...props.attributes } >{ props.children }</u>
-    )
-}
+// export const UnderlineLeaf = (props) => {
+//     return (
+//         <u { ...props.attributes } >{ props.children }</u>
+//     )
+// }
 
-export const DefaultLeaf = (props) => {
-    return (
-        <span {...props.attributes} >{props.children}</span>
-    )
+// export const DefaultLeaf = (props) => {
+//     return (
+//         <span {...props.attributes} >{props.children}</span>
+//     )
+// }
+
+const Leafs = ({ attributes, children, leaf }) => {
+    if(leaf.bold) {
+        children = <strong>{ children }</strong>   
+    }
+
+    if(leaf.italic) {
+        children = <em>{ children }</em>   
+    }
+
+    if(leaf.underline) {
+        children = <u>{ children }</u>
+    }
+
+    return <span { ...attributes } >{ children }</span>
 }
